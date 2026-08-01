f2a62e629254626b494e900ed8135998ea149c9d
praxantpacific <praxantpacific@gmail.com>
2026-08-01 21:20:56 +0545

    Add splash screen and app icon

---
+++ apkproject/app/src/main/java/com/example/sequenceiq/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.sequenceiq;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashActivity extends Activity {
+
+    private static final long SPLASH_DELAY_MS = 1400;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, SPLASH_DELAY_MS);
+    }
+}
